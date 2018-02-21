19bdd29842c9fb0910f86311ed8023b554f86b9b
Princeton Evans <princetonevans@yahoo.com>
2018-02-21 16:53:34 -0600

    fixed port

--- app.js
+++ app.js
@@ -43,5 +43,5 @@ app.post('/charge', (req, res) => {
 const port = process.env.Port || 5000;
 
 app.listen(port, () => {
-  console.log('Server started on port: ' + port);
+  console.log(`Server started on port ${port}`);
 });
