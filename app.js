3ced240ddb77343ef869961a140c4e80fdaa6863
Princeton Evans <princetonevans@yahoo.com>
2018-02-21 15:21:41 -0600

    added charge route

--- app.js
+++ app.js
@@ -8,15 +8,33 @@ const app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-// Body Parser Middleware
+//  Body Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-//Set Static Folder
+// Set Static Folder
 app.use(express.static(`${__dirname}/public`));
-//Index Route
+
+// Index Route
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+// Charge Route
+app.post('/charge', (req, res) => {
+  const amount = 999;
+
+  stripe.customers.create({
+    email: req.body.stripeEmail,
+    source: req.body.stripeToken
+  })
+  .then(customer => stripe.charges.create({
+    amount,
+    description: 'Web Development Ebook',
+    currency: 'usd',
+    customer:customer.id
+  }))
+  .then(charge => res.render('Success'));
 });
 
 const port = process.env.Port || 5000;
