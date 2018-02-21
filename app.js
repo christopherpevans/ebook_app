e991a5d49f66a163d9582ce5c53ac3b7ead7e1b3
Princeton Evans <princetonevans@yahoo.com>
2018-02-21 12:11:09 -0600

    added middleware

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+//Index Route
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
 const port = process.env.Port || 5000;
 
 app.listen(port, () => {
